ce6ccf49718b934f3af83a80bfeda2bfe11af4ef
Андрей Пономарев <nikitaraznoglazov@gmail.com>
2019-09-21 13:31:22 +0300

    all types Java

---
+++ src/main/java/lesson/Main.java
@@ -0,0 +1,18 @@
+package lesson;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Alloha world");
+    }
+    byte aByte=24; //8 bit
+    short bShort=1024; //16 bit
+    char cChar= 'a'; //16 bit
+    int dInt=25; //32 bit
+    long eLong; // 64 bit
+    float fFloat=3.4f; //32 bit
+    double gDouble=4.9f; //64 bit
+    boolean hBool=true; // 8 bit or 32 bit
+
+
+}
