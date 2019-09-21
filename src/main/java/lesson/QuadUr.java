ff3632f604998cd00a6ac803c5b6d49f99bb5c81
Quinton Link <lolkitty58@gmail.com>
2019-09-21 17:47:45 +0300

    Korni 2 ur

---
+++ src/main/java/lesson/QuadUr.java
@@ -0,0 +1,31 @@
+package lesson;
+import java.util.Scanner;
+public class QuadUr {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+
+        System.out.println("Input a: ");
+        int a=sc.nextInt();
+        System.out.println("Input b: ");
+        int b=sc.nextInt();
+        System.out.println("Input c: ");
+        int c=sc.nextInt();
+        double D=Math.sqrt((b*b)-4*a*c);
+
+        if(D>0){
+         double x1=(-b+D)/(2*a);
+         double x2=(-b-D)/(2*a);
+        System.out.println("x1="+x1);
+        System.out.println("x2="+x2);
+        }
+
+        if(D==0){
+            int x=-b/(2*a);
+            System.out.println("x="+x);
+        }
+        if(D<0){
+            System.out.println("Ar u gay");
+        }
+    }
+}
